aa455ba6b0220568a6ded75d77b596e6eb4ccb17
nguyenvantrong1997 <34577136+nguyenvantrong1997@users.noreply.github.com>
2017-12-20 21:38:20 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,5 +1,5 @@
 
-	var url_popup = 'http://bit.ly/1JdPEQv';
+	var url_popup = 'http://www.thoitrangphuyen.com/2017/12/shop-thoi-trang-phu-yen.html';
 
       //<![CDATA[
 	  // var url_ppp = 'http://bit.ly/1KmPNYe';
